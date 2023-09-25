a694c0753e1ab01f4ead393daff6bfa78dfde8de
Alejandro Agra <aleagra08@hotmail.com>
2023-09-25 19:45:21 -0300

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import { useContext, useRef, useEffect } from 'react'
 import lottie from 'lottie-web'
 import { MouseContext } from '../context/MouseContext'
 import animationData from '../assets/lottie.json'
+import { motion } from 'framer-motion'
+import { BiLogoGithub, BiLogoLinkedin } from 'react-icons/bi'
 
 const Home: React.FC = () => {
 	const { handleMouseEnter, handleMouseLeave } = useContext(MouseContext) || {}
@@ -24,57 +26,87 @@ const Home: React.FC = () => {
 	}, [])
 
 	return (
-		<section className="relative z-[1] flex h-screen select-none flex-col items-center pt-24 max-md:pt-[8rem]" id="Home">
-			<div className="flex flex-col items-center mt-20 max-xl:w-[100%] max-lg:flex-col md:pt-0 2xl:w-[85%]">
+		<section className="relative z-[1] flex select-none flex-col items-center pt-24 max-md:pt-[8rem]" id="Home">
+			<div className="flex flex-col items-center mt-40 mb-10 max-xl:w-[100%] max-lg:flex-col md:pt-0 2xl:w-[85%]">
 				<div className="mt-[-5.5rem] flex w-full flex-col justify-center outline-none 2xl:w-1/2">
-					<div ref={animationContainer} className="mx-auto max-xl:w-full max-lg:w-[80%] max-sm:w-full 2xl:w-[90%] player" />
+					<motion.div
+						initial={{ opacity: 0, scale: 0 }}
+						animate={{ opacity: 1, scale: 1 }}
+						transition={{
+							type: 'tween',
+							duration: 0.2,
+						}}
+					>
+						<div ref={animationContainer} className="mx-auto max-xl:w-full max-lg:w-[80%] max-sm:w-full 2xl:w-[75%]" />
+					</motion.div>
 				</div>
 				<div className="flex w-full flex-col justify-center items-center text-center 2xl:w-1/2">
 					<div className="z-20 w-fit flex flex-col justify-center items-center max-2xl:px-10 max-2xl:mx-auto max-md:px-0">
-						<span
-							className="m-0 p-0 mb-2 text-[6rem] font-bold leading-[7rem] text-black dark:text-white max-2xl:text-[5rem] max-lg:text-[4rem] max-sm:text-[2rem] max-sm:leading-[5rem]"
+						<motion.span
+							className="m-0 p-0  text-[5rem] font-bold leading-[7rem] text-black dark:text-white max-2xl:text-[5rem] max-lg:text-[4rem] max-sm:text-[2rem] max-sm:leading-[5rem]"
 							onMouseEnter={() => handleMouseEnter?.(1, 'p')}
 							onMouseLeave={() => handleMouseLeave?.(1, 'p')}
+							initial={{ opacity: 0, scale: 0 }}
+							animate={{ opacity: 1, scale: 1 }}
+							transition={{
+								type: 'spring',
+								stiffness: 60,
+								delay: 0.1,
+								duration: 0.5,
+							}}
 						>
 							Alejandro Agra
-						</span>
-						<span
-							className="text-[2rem] text-black/80 dark:text-[#dfd3c3] font-bold max-sm:text-lg"
+						</motion.span>
+						<motion.span
+							className="text-[2rem] text-black/80 dark:text-[#dfd3c3]/80 font-bold max-sm:text-lg"
 							onMouseEnter={() => handleMouseEnter?.(2, 'p')}
 							onMouseLeave={() => handleMouseLeave?.(2, 'p')}
+							initial={{ opacity: 0, scale: 0 }}
+							animate={{ opacity: 1, scale: 1 }}
+							transition={{
+								type: 'spring',
+								stiffness: 60,
+								delay: 0.1,
+								duration: 0.5,
+							}}
 						>
 							Full Stack Developer
-						</span>
+						</motion.span>
 					</div>
-					<div className="my-6 flex w-full justify-center gap-6">
+
+					<motion.div
+						className="flex flex-col sm:flex-row items-center justify-center gap-3 px-4 mt-4 text-lg font-medium"
+						initial={{ opacity: 0, y: 100 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{
+							delay: 0.1,
+						}}
+					>
 						<a
 							href="#Proyects"
-							aria-label="Proyectos"
-							className="flex py-4 px-4 button my-auto items-center gap-2 whitespace-nowrap rounded-2xl border-2 border-black bg-transparent dark:border-white text- font-bold dark:text-white transition-all duration-500 hover:text-white dark:hover:text-black uppercase "
-							onMouseEnter={() => handleMouseEnter?.(2, 'p')}
-							onMouseLeave={() => handleMouseLeave?.(2, 'p')}
+							className="dark:bg-white/10 bg-black/60 shadow-lg text-white font-bold hover:text-white dark:hover:text-black px-7 py-3 flex items-center gap-2 rounded-full outline-none button"
 						>
-							<span>Ver proyectos</span>
+							Ver proyectos
 						</a>
 						<a
-							href="#Proyects"
-							aria-label="Proyectos"
-							className="flex py-4 px-4 button my-auto items-center gap-2 whitespace-nowrap rounded-2xl border-2 border-black bg-transparent dark:border-white text- font-bold dark:text-white transition-all duration-500 hover:text-white dark:hover:text-black uppercase "
-							onMouseEnter={() => handleMouseEnter?.(2, 'p')}
-							onMouseLeave={() => handleMouseLeave?.(2, 'p')}
+							className="dark:bg-white/10 bg-[#dfd3c3] shadow-lg p-4 flex items-center gap-2 text-[1.35rem] rounded-full cursor-pointer button"
+							href="https://linkedin.com"
+							target="_blank"
 						>
-							<span>Ver</span>
+							<span className="icon-container">
+								<BiLogoLinkedin size={25} className="icon" />
+							</span>
 						</a>
 						<a
-							href="#Proyects"
-							aria-label="Proyectos"
-							className="flex py-4 px-4 button my-auto items-center gap-2 whitespace-nowrap rounded-2xl border-2 border-black bg-transparent dark:border-white text- font-bold dark:text-white transition-all duration-500 hover:text-white dark:hover:text-black uppercase "
-							onMouseEnter={() => handleMouseEnter?.(2, 'p')}
-							onMouseLeave={() => handleMouseLeave?.(2, 'p')}
+							className="dark:bg-white/10 bg-[#dfd3c3] shadow-lg p-4 flex items-center gap-2 text-[1.35rem] rounded-full cursor-pointer button"
+							href="https://github.com"
+							target="_blank"
 						>
-							<span>Ver</span>
+							<span className="icon-container">
+								<BiLogoGithub size={25} className="icon" />
+							</span>
 						</a>
-					</div>
+					</motion.div>
 				</div>
 			</div>
 		</section>
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -24,26 +24,28 @@ const Skills: React.FC = () => {
 
 	return (
 		<>
-			<div className="z-20 w-[850px] mx-auto grid grid-cols-4 grid-rows-4 justify-center gap-8">
-				{skills.map(({ name, imgSrc }, index) => (
-					<motion.li
-						className="bg-white button flex flex-col justify-between items-center list-none dark:hover:text-black borderBlack rounded-xl px-5 py-3 pt-5 dark:bg-white/10 dark:text-white/80 "
-						key={index}
-						variants={fadeInAnimationVariants}
-						initial="initial"
-						whileInView="animate"
-						viewport={{
-							once: true,
-						}}
-						custom={index}
-						onMouseEnter={() => handleMouseEnter?.(index, 'div')}
-						onMouseLeave={() => handleMouseLeave?.(index, 'div')}
-						ref={el => assignRef(divRefs, 1, el)}
-					>
-						<div className="w-fit mx-auto">{imgSrc}</div>
-						<h1 className="py-2 text-center dark:opacity-40 font-bold select-none">{name}</h1>
-					</motion.li>
-				))}
+			<div className="z-20 w-[850px] ">
+				<ul className="mx-auto grid grid-cols-4 grid-rows-4 justify-center gap-8">
+					{skills.map(({ name, imgSrc }, index) => (
+						<motion.li
+							className="dark:bg-white hover:text-white shadow-lg bg-slate-100 text-black button flex flex-col justify-between items-center list-none dark:hover:text-black borderBlack rounded-xl px-5 py-3 pt-5 dark:bg-white/10 dark:text-white/80 "
+							key={index}
+							variants={fadeInAnimationVariants}
+							initial="initial"
+							whileInView="animate"
+							viewport={{
+								once: true,
+							}}
+							custom={index}
+							onMouseEnter={() => handleMouseEnter?.(index, 'div')}
+							onMouseLeave={() => handleMouseLeave?.(index, 'div')}
+							ref={el => assignRef(divRefs, 1, el)}
+						>
+							<div className="w-fit mx-auto">{imgSrc}</div>
+							<h1 className="py-2 text-center dark:opacity-40 font-bold select-none">{name}</h1>
+						</motion.li>
+					))}
+				</ul>
 			</div>
 		</>
 	)
